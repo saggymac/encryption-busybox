a1eb0f6e8c7c8aebc1e6fcf76dd148f9596ce9bb
Mike <junkmail4mjd@gmail.com>
2018-07-26 19:37:31 -0400

    created kubernetes specs

--- encryptionHandlers/decrypt.js
+++ encryptionHandlers/decrypt.js
@@ -5,24 +5,45 @@
  *
  */
 function getDecryptionHandler( jose ) {
-    return function( req, res ) {
+    return async function( req, res ) {
+        console.log("Type of req.body: ", typeof req.body);
+        console.log("Type of req.body.key: ", typeof req.body.key);
+        console.log("Type of req.body.jwe: ", typeof req.body.jwe);
         if ( !req.body.key ) {
-            res.send( { "message": "No Decryption Key Supplied" } );
+            console.log("Type of req.body.key: ", typeof req.body.key);
+            res.status(400).send( { "message": "No Decryption Key Supplied" } );
         } else if ( !req.body.jwe ) {
-            res.send( { "message": "No JWE to decrypt" } );
+            console.log("Type of req.body.jwe: ", typeof req.body.jwe);
+            res.status(400).send( { "message": "No JWE to decrypt" } );
         } else {
-            jose.JWK.asKey( req.body.key )
+            console.log("Importing Key : ", JSON.stringify( req.body.key));
+            let key = await jose.JWK.asKey( req.body.key );
+            console.log("Import Key returned key: ", typeof key);
+            let decryptor = await jose.JWE.createDecrypt( key );
+            let result = await decryptor.decrypt( req.body.jwe );
+            let results = {
+                plaintext: result.plaintext.toString( 'utf8' )
+            };
+            res.send( results );
+
+
+            /*jose.JWK.asKey( req.body.key )
             .then( function( key ) {
-                jose.JWE.createDecrypt( key )
-                    .decrypt( req.body.jwe )
-                    .then( function( result ) {
-                        let results = {
-                            plaintext: result.plaintext.toString( 'utf8' )
-                        };
-                        res.send( results );
-                    });
-                } 
-            );
+                console.log("Import Key returned key: ", typeof key);
+                if (key ){
+                  jose.JWE.createDecrypt( key )
+                      .decrypt( req.body.jwe )
+                      .then( function( result ) {
+                          let results = {
+                              plaintext: result.plaintext.toString( 'utf8' )
+                          };
+                          res.send( results );
+                      });
+                  }
+                else {
+                  res.status(400).send( { "message": "key import failed" } );
+                }
+            });*/
         }
     };
 }
